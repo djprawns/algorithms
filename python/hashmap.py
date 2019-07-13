e86c42d03f6bef0053e6116446383666bddaf7e1
Pranav Sharma <pranavsharma.sre@gmail.com>
2019-07-14 02:17:08 +0530

    Deleting some files & writing hashmap

--- python/hashmap.py
+++ python/hashmap.py
@@ -1,3 +1,39 @@
-import requests
+class Node():
 
-r = requests.post('http://localhost:9090/fabreferralservice/add/referral', data = {"code":"PRANAV", "userId":"32613510-d797-4f27-b164-f6e7449d5739"})
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+
+class HashMap():
+
+	capacity = 10
+	load_factor = 0.7
+
+	def __init__(self):
+		self.bucket = [None]*10
+		self.capacity = capacity
+		self.items = 0
+
+	def put(self, obj):
+		key = hashkey(obj)
+		if(!self.bucket[key]):
+			self.bucket[key] = Node(obj)
+			return obj
+		next = self.bucket[key]
+		curr = None
+		while next:
+			curr = next
+			next = next.next
+		curr.next = Node(obj)
+		return obj
+
+	def hashkey(self, obj):
+		return self.hashcode(obj) % HashMap.capacity
+
+	def hashcode(obj):
+		return id(obj)
+
+	@classmethod
+	def check_if_rehash_required(cls, capacity):
+		if(cls.load_factor*capacity)
